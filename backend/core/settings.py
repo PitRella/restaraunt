25ab8ac4d114d48d7484808f76efaed46d76baf9
Pit-Rella <serezha.sliva@gmail.com>
2025-03-18 15:13:44 +0200

    - Added generation JWT token in urls

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -1,11 +1,9 @@
 from pathlib import Path
+from datetime import timedelta
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 SECRET_KEY = 'django-insecure--=3dnvx%(&+*slb-w-(4-vpv)=_=@vj*+60f!6xql(jmz78lnb'
-
 DEBUG = True
-
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
@@ -33,9 +31,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'core.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -53,7 +49,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'core.wsgi.application'
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -62,6 +57,10 @@ DATABASES = {
 }
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+
 }
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -77,7 +76,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+SIMPLE_JWT = {
+}
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,7 +5,10 @@ from rest_framework import routers
 
 from menu.views import ProvisionViewSet, MenuViewSet
 from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from user.views import UserViewSet
 
 router = routers.DefaultRouter()
@@ -14,8 +17,14 @@ router.register(r"menu", MenuViewSet)
 router.register(r"user", UserViewSet)
 
 urlpatterns = [
+    # admin
     path('admin/', admin.site.urls),
+    # api
     path("api/", include(router.urls)),
+    # jwt
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # swagger
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
 ]
