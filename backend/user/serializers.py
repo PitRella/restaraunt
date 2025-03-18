fe2547153316a387dec9ccad63e2f54b22062b0a
Pit-Rella <serezha.sliva@gmail.com>
2025-03-18 15:07:02 +0200

    - Installed JWT - Added Order app to admin panel - Created UserRegistrationSerializer

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -1,11 +1,40 @@
-from rest_framework.serializers import ModelSerializer
-
+from django.contrib.auth.password_validation import validate_password
+from rest_framework.serializers import ModelSerializer, CharField, EmailField
 from user.models import CustomUser
-
+from django.core.exceptions import ValidationError
 
 class UserOutSerializer(ModelSerializer):
-
     class Meta:
         model = CustomUser
         fields = ('email', 'first_name', 'last_name', 'avatar')
         read_only_fields = ('email',)
+
+
+class UserRegisterSerializer(ModelSerializer):
+    password = CharField(write_only=True, required=True)
+    retype_password = CharField(write_only=True, required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ("email", "first_name", "last_name", "avatar", "password", "retype_password")
+        read_only_fields = ("email",)
+
+    def validate_email(self, value):
+        if CustomUser.objects.filter(email=value).exists():
+            raise ValidationError("Этот email уже зарегистрирован.")
+        return value
+    def validate(self, data):
+        password = data.get("password")
+        retype_password = data.get("retype_password")
+
+        if password != retype_password:
+            raise ValidationError({"retype_password": "Пароли не совпадают."})
+
+        validate_password(password)
+
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop("retype_password")
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
